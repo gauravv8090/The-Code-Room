396d8ec146705d01e709502e499f05c62c9aaf8f
gauravv8090 <gaurav8090983461@gmail.com>
2023-01-05 16:17:05 +0530

    Frontend done

--- coderoom-frontend/src/Pages/EditorPage.jsx
+++ coderoom-frontend/src/Pages/EditorPage.jsx
@@ -1,11 +1,44 @@
-
-
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import logo from "../Assets/Code2.png";
+import Clients from "../components/Clients";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return (
-    <div>Editor Page</div>
-  )
-}
+  const [clients, setClients] = useState([
+    { socketId: 1, userName: "Gaurav A" },
+    { socketId: 2, userName: "Rakesh K" },
+    { socketId: 3, userName: "Mukesh K" },
+  ]);
 
-export default EditorPage
+  return (
+    <div className="MainWrapper">
+      <div className="left">
+        <div className="leftInner">
+          <div className="logo">
+            <img className="logoImage" src={logo} alt="logo" />
+          </div>
+
+          <h3>Connected</h3>
+          <div className="clientList">
+            {clients.map((client) => (
+              <Clients key={client.socketId} userName={client.userName} />
+            ))}
+          </div>
+        </div>
+
+        <button className="btn copyBtn" >
+          Copy ROOM ID
+        </button>
+        <button  className="btn leaveBtn" >
+          Leave
+        </button>
+      </div>
+      <div className="editorWrap">
+        <Editor/>
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
